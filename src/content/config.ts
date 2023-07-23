0975e6a17c4444abef9665880a0ff81087678af1
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-22 22:16:10 -0300

    feat(posts): add new post "Foliage Classification" fix(posts): fix typo in post "How do I structure my C++ projects" fix(config.ts): change variable name from postsCollection to posts fix(server.ts): change port variable case from lowercase port to uppercase PORT feat(server.ts): add support for process.env.PORT environment variable feat(public): add favicon and site manifest files feat(content): add new post "Foliage Classification" feat(content): add cover and updatedAt fields to posts schema feat(content): add series collection to config feat(content): add seriesId and orderInSeries fields to posts schema
    chore(posts): delete untitled post
    feat(posts): add first post in first series
    feat(series): add first series
    fix(styles): remove underline from post links
    fix(styles): remove unused font-family from body

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,31 @@
 import { defineCollection, z } from "astro:content";
 
-const postsCollection = defineCollection({
+const posts = defineCollection({
   type: "content",
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
-      publishedAt: z.date(),
-      description: z.string(),
-      isPublish: z.boolean(),
-      isDraft: z.boolean().default(false),
       cover: image().optional(),
       coverAlt: z.string().optional(),
+
+      title: z.string(),
+      description: z.string(),
+      featured: z.boolean().default(false),
+      publishedAt: z.date(),
+      updatedAt: z.date().optional(),
+
+      seriesId: z.string().optional(),
+      orderInSeries: z.number().optional(),
+      isDraft: z.boolean().default(false),
     }),
 });
 
-export const collections = { posts: postsCollection };
+const series = defineCollection({
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string(),
+    featured: z.boolean().default(false),
+  }),
+});
+
+export const collections = { posts, series };
