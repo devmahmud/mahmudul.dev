256395ff079446810d6356b187757346e1fb2265
Mahmudul Alam <expelmahmud@gmail.com>
2023-09-12 20:10:42 -0300

    chore(astro.config.ts): remove unused imports and reformat code for better readability feat(astro.config.ts): add support for @astrojs/partytown plugin to enable Google Analytics tracking feat(package.json): add @astrojs/partytown as a dependency feat(SEOTags.astro): import and include Analytics component to enable Google Analytics tracking feat(analytics.astro): create analytics component to include Google Analytics tracking code

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import fs from "fs";
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
@@ -7,14 +6,12 @@ import prefetch from "@astrojs/prefetch";
 import preact from "@astrojs/preact";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-
 import rehypePrettyCode from "rehype-pretty-code";
 import type { Options as PrettyCodeOptions } from "rehype-pretty-code";
 import Icons from "unplugin-icons/vite";
-
 import { SITE_URL } from "./src/data/config";
 import { remarkReadingTime } from "./src/plugins";
-
+import partytown from "@astrojs/partytown";
 const prettyCodeOptions: PrettyCodeOptions = {
   keepBackground: false,
   theme: JSON.parse(
@@ -33,6 +30,11 @@ export default defineConfig({
     preact({
       compat: true,
     }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
   markdown: {
     syntaxHighlight: false,
@@ -43,7 +45,7 @@ export default defineConfig({
   adapter: vercel({
     analytics: true,
     imageService: true,
-    functionPerRoute: false
+    functionPerRoute: false,
   }),
   vite: {
     plugins: [
