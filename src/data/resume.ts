57e31dc7c22a45fade3f2992086b089637bb3eea
Mahmudul Alam <expelmahmud@gmail.com>
2023-09-19 16:21:32 +0600

    feat: resume page

---
+++ src/data/resume.ts
@@ -0,0 +1,229 @@
+const resume = {
+  profile: {
+    name: 'Mahmudul Alam',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/19981097?v=4',
+  },
+  resumeURL: 'https://drive.google.com/file/d/1Xp79ZiYGd2RvjEoWO63CTaFrQBASRiRx/view',
+  links: {
+    linkedIn: 'https://www.linkedin.com/in/devmahmud/',
+  },
+  skills: [
+    {
+      category: 'Programming Languages',
+      stacks: [
+        {
+          name: 'TypeScript and JavaScript',
+          icon: '/images/typescript.svg',
+          rate: 5,
+        },
+        {
+          name: 'Python',
+          icon: '/images/python.png',
+          rate: 5,
+        },
+        { name: 'Golang', icon: '/images/golang.svg', rate: 3 },
+        {
+          name: 'Shell script',
+          icon: '/images/shell.svg',
+          rate: 4,
+        },
+        {
+          name: 'Solidity',
+          icon: '/images/solidity.svg',
+          rate: 3,
+        },
+      ],
+    },
+    {
+      category: 'Development Tools',
+      stacks: [
+        {
+          name: 'Linux',
+          icon: '/images/linux.png',
+          rate: 5,
+        },
+        {
+          name: 'Docker',
+          icon: '/images/docker.webp',
+          rate: 5,
+        },
+        {
+          name: 'Git',
+          icon: '/images/git.svg',
+          rate: 5,
+        },
+        {
+          name: 'Node.JS',
+          icon: 'https://cdn.iconscout.com/icon/free/png-512/node-js-1174925.png',
+          rate: 5,
+        },
+      ],
+    },
+    {
+      category: 'Frontend',
+      stacks: [
+        {
+          name: 'React JS',
+          icon: '/images/react.png',
+          rate: 5,
+        },
+        {
+          name: 'React Native',
+          icon: '/images/react-native.svg',
+          rate: 5,
+        },
+        {
+          name: 'Solid JS',
+          icon: '/images/solidjs.svg',
+          rate: 5,
+        },
+        {
+          name: 'Next JS',
+          icon: '/images/nextjs.svg',
+          rate: 5,
+        },
+        {
+          name: 'Astro',
+          icon: '/images/astro.svg',
+          rate: 5,
+        },
+        {
+          name: 'Svelte',
+          icon: '/images/svelte.svg',
+          rate: 5,
+        },
+        {
+          name: 'Redux, Redux Toolkit(RTK)',
+          icon: '/images/redux.png',
+          rate: 5,
+        },
+      ],
+    },
+    {
+      category: 'Backend',
+      stacks: [
+        {
+          name: 'Node JS',
+          icon: '/images/nodejs.png',
+          rate: 5,
+        },
+        {
+          name: 'Django',
+          icon: '/images/django.svg',
+          rate: 5,
+        },
+        {
+          name: 'Redis',
+          icon: '/images/redis.svg',
+          rate: 5,
+        },
+        {
+          name: 'Mongo DB',
+          icon: '/images/mongodb.png',
+          rate: 5,
+        },
+        {
+          name: 'Postgresql',
+          icon: '/images/postgresql.svg',
+          rate: 5,
+        },
+      ],
+    },
+    {
+      category: 'Infra',
+      stacks: [
+        {
+          name: 'AWS Services (ecs, ecs, lambda, sns, sqs, dynamodb, others)',
+          icon: '/images/aws-logo.svg',
+          rate: 5,
+        },
+        {
+          name: 'Vercel',
+          icon: '/images/vercel.svg',
+          rate: 5,
+        },
+      ],
+    },
+  ],
+  experiences: [
+    {
+      company: 'Leveor',
+      jobTitle: 'Founding Engineer',
+      location: 'Dhaka, Bangladesh (Hybrid)',
+      datesEmployed: 'Jul 2021 – present',
+      techStack: [
+        'react',
+        'react-native',
+        'Next.js',
+        'Web3.js',
+        'TypeScript',
+        'Python',
+        'JavaScript',
+        'redux',
+        'redux-toolkit',
+        'docker',
+      ],
+      description:
+        'Building User Interfaces using React.js, Next.js, Typescript. Integrating smart contracts with frontend UI. Integrating Rest APIs with frontend UI. Developing REST APIs with Django REST Framework & Express. Real-time Voice & Video chat with Socket & WebRTC',
+    },
+    {
+      company: 'Universal Machine',
+      jobTitle: 'Software Engineer',
+      location: 'Santa Clara, California, United States (Remote)',
+      datesEmployed: 'Sep 2021 – Present',
+      techStack: [
+        'react',
+        'react-native',
+        'Next.js',
+        'Web3.js',
+        'TypeScript',
+        'Python',
+        'JavaScript',
+        'redux',
+        'redux-toolkit',
+        'docker',
+        'Graphql',
+        'Expo',
+      ],
+      description:
+        'Building User Interfaces using React.js, Next.js, Typescript. Working on Smart Contract & Frontend Integrations. Working on REST API Integrations. Building cross-platform native apps using React Native. Working on BLE, NFC, & QR CODE',
+    },
+    {
+      company: 'Heavytask LLC',
+      jobTitle: 'Software Engineer',
+      location: 'Dhaka, Bangladesh',
+      datesEmployed: 'Feb 2020 – Jan 2023',
+      techStack: [
+        'Next.js',
+        'Redux.js',
+        'TypeScript',
+        'React JS',
+        'JavaScript',
+        'Bootstrap',
+        'React.js',
+        'Redux',
+      ],
+      description:
+        'Initiated & bootstrapped a maintainable & scalable codebase for a global CRM project. Designed & developed component specifications based on the Product Requirement Document received from the client. Led the planning of reusability design of each component to meet the design requirement. Developed design-critical components to implement drag-and-drop Kanban board, smooth Digital Signature pad, complex reporting analytics dashboard, complicated 1:1 PDF generation web interface for Invoicing module, etc. Implemented required data model functionalities for every component & feature module. Test, streamline & scale each component development to enhance Customer Experience. Enforce best practices in version control & code reusability',
+    },
+  ],
+  educations: [
+    {
+      institution: 'Daffodil International University',
+      fieldOfStudy: 'Computer Science and Engineering',
+      period: '2015-2019',
+    },
+    {
+      institution: 'New Govt. Degree College, Rajshahi',
+      fieldOfStudy: 'Higher Secondary School Certificate',
+      period: '2012 – 2014',
+    },
+    {
+      institution: 'Naogaon K.D Govt. High School',
+      fieldOfStudy: 'Secondary School Certificate',
+      period: '2011 – 2012',
+    },
+  ],
+};
+
+export default resume;
