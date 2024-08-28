37753fa3442c373f63dab8f890687ccca43fd501
Mahmudul Alam <mahmudul@ccprotocol.xyz>
2024-08-28 11:57:50 +0600

    fix: experience years

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,7 +14,7 @@ const presentation: Presentation = {
   mail: 'expelmahmud@gmail.com',
   title: 'Hi, I’m <Mahmudul::Alam> 👋',
   description:
-    "*Hyālō!* I'm a seasoned full-stack developer with 3+ years of experience in consumer-facing software development. I excel in designing and implementing solutions, including CRM and CMS for B2B clients. My expertise lies in JavaScript (ReactJS, React Native, NodeJS) and Python (Django, Web3.js, Ethers.js), and I prioritize clean, reusable code in a well-structured architecture. I'm also active in open-source contributions.",
+    "*Hyālō!* I'm a seasoned full-stack developer with 5+ years of experience in consumer-facing software development. I excel in designing and implementing solutions, including CRM and CMS for B2B clients. My expertise lies in JavaScript (ReactJS, React Native, NodeJS) and Python (Django, Web3.js, Ethers.js), and I prioritize clean, reusable code in a well-structured architecture. I'm also active in open-source contributions.",
   socials: [
     {
       label: 'Twitter',
