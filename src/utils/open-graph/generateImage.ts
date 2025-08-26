6e51e8567ab687cd0c2a268f0117056de1602edd
Mahmudul Alam <mahmudul@ccprotocol.xyz>
2025-08-26 15:26:32 +0600

    feat: enhance SEO tags with article metadata and update Open Graph image dimensions

--- src/utils/open-graph/generateImage.ts
+++ src/utils/open-graph/generateImage.ts
@@ -1,8 +1,8 @@
-import sharp from "sharp";
-import { readFile } from "node:fs/promises";
-import satori from "satori";
-import type { SatoriOptions } from "satori";
-import { OpenGraphTemplate } from "./template";
+import sharp from 'sharp';
+import { readFile } from 'node:fs/promises';
+import satori from 'satori';
+import type { SatoriOptions } from 'satori';
+import { OpenGraphTemplate } from './template';
 
 export const generateOgImage = async ({
   title,
@@ -14,21 +14,21 @@ export const generateOgImage = async ({
   date: Date;
 }): Promise<Buffer> => {
   const options: SatoriOptions = {
-    width: 600,
-    height: 315,
+    width: 1200,
+    height: 630,
     embedFont: true,
     fonts: [
       {
-        name: "JetBrainsMono",
-        data: await readFile("./public/fonts/JetBrainsMono-Bold.ttf"),
+        name: 'JetBrainsMono',
+        data: await readFile('./public/fonts/JetBrainsMono-Bold.ttf'),
         weight: 600,
-        style: "normal",
+        style: 'normal',
       },
       {
-        name: "NotoSansJP",
-        data: await readFile("./public/fonts/NotoSansJP-Bold.ttf"),
+        name: 'NotoSansJP',
+        data: await readFile('./public/fonts/NotoSansJP-Bold.ttf'),
         weight: 600,
-        style: "normal",
+        style: 'normal',
       },
     ],
   };
